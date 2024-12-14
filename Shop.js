a8fd247c2f85ca5dba82b8b02c4e2d58e459e383
yousefadel2003 <113187316+yousefadel2003@users.noreply.github.com>
2024-12-14 06:56:24 +0200

    Add files via upload

---
+++ Shop.js
@@ -0,0 +1,5 @@
+// Toggle Hamburger Menu
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
